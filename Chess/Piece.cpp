6049192b5a895439a4280a8d4c474b9f105dd4ab
Sutka998 <sutka998@gmail.com>
2018-04-28 21:07:17 +0200

    Recreating move-hit logic. Added functions to check wether the piece able to move in the given position. The check caches the position, when you are moving the piece, it checks the cache first.

--- Chess/Piece.cpp
+++ Chess/Piece.cpp
@@ -3,26 +3,46 @@
 namespace ch {
 	
 	Piece::Piece(Color color, const Position& position, PieceName pn) 
-		: m_color(color), m_position(position), pieceType(pn),
-		m_isMoved(false), m_mGridEvaled(false), m_hGridEvaled(false)
+		: m_color(color), m_position(position), pieceType(pn), 
+		m_isMoved(false), m_mGridEvaled(false), m_hGridEvaled(false), m_isCacheValid(false)
 	{}
 
-	bool Piece::isInGrid(const std::vector<Position>& m_grid, const Position& pos)
-	{
-		for (auto iter = m_grid.begin(); iter != m_grid.end(); iter++)	{ 
+	bool Piece::canMoveHit(const Position& pos, MovType mvtyp) const {
+		//Is there any cached value
+		if(m_isCacheValid && (pos == m_canMoveHitCache)){
+			return true;
+		}
+		if(m_isInGrid(mvtyp, pos)) { //Then check for the grid elements
+			m_canMoveHitCache = pos;
+			m_isCacheValid = true;
+			return true;
+		}
+		return false;
+	} 
+
+	bool Piece::m_isInGrid(MovType mvtype, const Position& pos) const {
+		auto m_grid = &m_hitGrid;
+		if(mvtype == MovType::MOVE)	{m_grid = &m_moveGrid;}
+
+		for (auto iter = m_grid->begin(); iter != m_grid->end(); iter++)	{ 
 			if(*iter == pos) return true;
 		}
 		return false;
 	}
 
-	bool Piece::Move_Hit(const Position& pos) {
-		if(isInGrid(m_moveGrid, pos) || isInGrid(m_hitGrid, pos)) {
-			m_position = pos;
-			m_mGridEvaled = m_hGridEvaled = false; //We stepped, we have to evaluate grids again.
-			m_evalHitGrid(); //Moved, refreshing the grids.
-			m_evalMoveGrid();
-			return true;
+	void Piece::m_move(const Position& pos) {
+		m_position = pos;
+		m_mGridEvaled = m_hGridEvaled = false;//The grids no longer evaluated, it needs to be false, for refreshing the grids.
+		m_evalHitGrid(); //Moved, refreshing the grids.
+		m_evalMoveGrid();
+		m_isCacheValid = false; //the cached moving position is no longer valid.
+	}
+
+	void Piece::Move_Hit(const Position& pos) {
+		if(canMoveHit(pos, MovType::HIT) || canMoveHit(pos, MovType::MOVE)) {
+			m_move(pos);
+			return;
 		}
-		return false;
+		throw std::invalid_argument("Position is not in the grid. Unable to move the piece.");
 	}
 }
--- Chess/Piece.h
+++ Chess/Piece.h
@@ -9,12 +9,18 @@ namespace ch {
 	enum class PieceName {
 		PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING
 	};
+	enum class MovType{
+		MOVE, HIT
+	};
 
 	class Piece {
 	private:
 		//Depends on the type of piece
 		virtual void m_evalHitGrid() = 0;
 		virtual void m_evalMoveGrid() = 0;
+		bool m_isInGrid(MovType, const Position&) const;
+
+		void m_move(const Position&);
 	protected:
 		//Have hitGrid, or moveGrid been evaluated since the last step
 		bool m_mGridEvaled;
@@ -23,6 +29,9 @@ namespace ch {
 		bool m_isMoved;
 		Position m_position;
 		const Color m_color;
+
+		mutable bool m_isCacheValid;
+		mutable	Position m_canMoveHitCache;
 		//Movements that are can be done
 		//Contains the coordinates, where the piece can step, no repeatings.
 		std::vector<Position> m_hitGrid;
@@ -31,9 +40,13 @@ namespace ch {
 		const PieceName pieceType;
 		Piece(Color, const Position&, PieceName);
 
-		static bool isInGrid(const std::vector<Position>&, const Position&);
+
+
+		bool canMoveHit(const Position&, MovType) const;
+		bool canHit(const Position&) const;
+		virtual void Move_Hit(const Position&);
+
 		//Getters
-		virtual bool Move_Hit(const Position&);
 		const std::vector<Position>& getHitGrid() const {return m_hitGrid;}
 		const std::vector<Position>& getMoveGrid() const {return m_moveGrid;}
 		const Position& getPosition() const { return m_position;}
