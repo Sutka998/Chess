7e64c40dde7ff74e1fe13efe7eb31294b5869476
Sutka998 <sutka998@gmail.com>
2018-04-29 21:41:19 +0200

    Completing Board class

--- Chess/Board.cpp
+++ Chess/Board.cpp
@@ -1,5 +1,45 @@
 #include "Board.h"
 
 namespace ch {
-	
+
+	Board::Board()
+		: m_whiteCount(0), m_blackCount(0)
+	{
+		for (int i = 0; i < 64; i++) {
+			m_pieceArray[i] = nullptr;
+		}
+	}
+
+	void Board::deletePieceAt(const Position& pos) {
+		Piece* piecPos = m_at(pos);
+		delete piecPos;
+		piecPos = nullptr;
+	}
+
+	void Board::movePiece(const Position& from, const Position& dest) {
+		Piece* destPtr = m_at(dest);
+		if(destPtr == nullptr) {
+			Piece* fromPtr = m_at(from);
+			destPtr = fromPtr;
+			fromPtr = nullptr;
+			return;
+		}
+		throw std::logic_error("Destination is not nullptr.");
+	}
+
+	const ch::Piece* Board::getPieceAt(const Position& pos) const {
+		return m_at(pos);
+	}
+
+	Board::~Board() {
+		try {
+			for (int i = 0; i < 64 ; i++) {
+				delete m_pieceArray[i];
+			}
+		}
+		catch(...) {
+
+		}
+	}
+
 }
--- Chess/Board.h
+++ Chess/Board.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Piece.h"
+#include <vector>
 
 namespace ch {
 	class Board {
@@ -7,12 +8,37 @@ namespace ch {
 		Piece* m_pieceArray[64];
 		unsigned short m_whiteCount;
 		unsigned short m_blackCount;
-	public:
-		Board() {};
+		Piece* m_at(const Position& pos) const{
+			return m_pieceArray[pos.getRow()*8 + pos.getColumn().number];}//0th row, 0th column is the beginning
 
-		bool placePieceAt(const Position&, PieceName);
-		bool deletePieceAt(const Position&);
-		const Piece& getPieceAt(const Position&) const;
+		Board(const Board&);
+	public:
+		Board();
+
+		template<class PIECE_T>
+		void placePieceAt(Color color, const Position& pos) {
+			Piece* posPtr = m_at(pos);
+			if(posPtr == nullptr) {
+				try {
+					posPtr = new PIECE_T(color, pos);
+				}
+				catch(...) {
+					delete posPtr;
+					posPtr = nullptr;
+					throw;
+				}
+				if (color == Color::BLACK){m_blackCount++;	}
+				else{m_whiteCount++;}
+				return;
+			}
+			throw std::logic_error("Destination is not nullptr.");
+		}
+
+		void deletePieceAt(const Position&);
+
+		void movePiece(const Position& from, const Position& dest);
+		const Piece* getPieceAt(const Position&) const;
 		
+		~Board();
 	};
 }
