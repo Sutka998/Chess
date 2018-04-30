d70d2f7b03adc4b66e4e5cabc8e8b8cbb10f4251
Sutka998 <sutka998@gmail.com>
2018-04-30 02:18:15 +0200

    "Where can the player step" prediction

--- Chess/ChessEngine.cpp
+++ Chess/ChessEngine.cpp
@@ -1,7 +1,44 @@
 #include "ChessEngine.h"
 
 namespace ch {
-	ChessEngine::ChessEngine(void)
+
+	bool ChessEngine::m_specStepProcess(const Position& src, const Position& dest) {
+		auto srcPiece = m_Board.getPieceAt(src);
+		//Castling check and process
+		if((m_currCol == Color::WHITE && !m_whiteCastled) || (m_currCol == Color::BLACK && !m_blackCastled)) { //There is ability to castle
+			if(srcPiece->pieceType == PieceType::KING) { //King is chosen
+				if((m_currCol == Color::WHITE) && (dest.getRow() == 1)) { //White, in the first row
+					//Short castling, E to G and the way is free between the king and the rook: 
+					if(dest.getColumn().letter == Column::CL::G && m_isWayFree(src, Position(Column::CL::H, 1))) {
+						//TODO
+					}
+				}				
+			}
+		}
+		
+		return true;
+	}
+
+	bool ChessEngine::m_isWayFree(const Position& src, const Position& dest) {
+		return true;
+	}
+
+	void ChessEngine::m_calcNoCheckGrid() {
+		const King& cCKing = (m_currCol == Color::WHITE)? m_whiteKing : m_blackKing;
+		cCKing.noCheckGrid.clear(); //Clearing it's grid first
+		if (!m_activeCheck){ //The current player is not in check
+			auto mvGridKing = cCKing.getMoveGrid();
+			//For the KING
+			for (auto it = mvGridKing.begin(); it < mvGridKing.end(); it++) {
+				if(!m_isKingInCheck(*it, cCKing.getPosition())) { //When the king is not in check, iterating through the move grid
+					cCKing.noCheckGrid.push_back(*it); //Then it can move on that position
+				}
+			}
+
+		}
+	}
+
+	ChessEngine::ChessEngine(Board& chessBoard) : m_Board(chessBoard)
 	{
 	}
 
--- Chess/ChessEngine.h
+++ Chess/ChessEngine.h
@@ -1,13 +1,37 @@
 #pragma once
 #include "Piece.h"
+#include "Pawn.h"
+#include "Board.h"
+#include "King.h"
 namespace ch {
 	class ChessEngine {
 		Color m_currCol;
-		bool m_isIncheck();
-		bool m_isValidMove();
+		//True, when valid, Piece and color check
+		bool m_grabCheck(const Position& src) {  
+			auto piece = m_Board.getPieceAt(src);
+			return ((piece != nullptr) && (piece->getColor() == m_currCol)); //If there is a piece, and it's color is ok
+		}
+		//Checking for castling, en Passant, or pawn swap
+		bool m_specStepProcess(const Position& src, const Position& dest);
+		//Checks whether the way is free to the dest. Does not include destination.
+		bool m_isWayFree(const Position& src, const Position& dest);
+
+		const King& m_whiteKing;
+		const King& m_blackKing;
+
+		void m_calcNoCheckGrid();
+		bool m_isKingInCheck(const Position& targetPos, const Position& currPos);
+
+		bool m_activeCheck;
+
+		bool m_blackCastled;
+		bool m_whiteCastled;
+
+		Pawn* m_delEnPassCache; //EnPass able pawn stored here, deleted in the next round
+		Board& m_Board;
 	public:
-		ChessEngine(void);
-		bool Command(Position src, Position dst);
+		ChessEngine(Board& chessBoard);
+		bool ProcessStep(const Position& src, const Position& dst);
 		const Color& getCurrentColor();
 		~ChessEngine(void);
 	};
