2e5c544e58061a4984263cc08eddcb5f3be7c657
Sutka998 <sutka998@gmail.com>
2018-05-13 00:53:33 +0200

    test main loop

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,13 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include "position.h"
 
+#include "PiecesH.h"
 #include "Piece.h"
-#include "Pawn.h"
 #include "position.h"
 #include <iostream>
 #include <list>
 #include <string>
 #include <fstream>
+#include "Board.h"
+#include "ChessEngine.h"
+#include "Graphics.h"
+#include <sstream>
 
 namespace ch{
 	template <class _FPtr>
@@ -36,11 +40,24 @@ namespace ch{
 		void operator+=(_FPtr fvP){
 			m_toCall.push_back(fvP);
 		}
-		
+		void operator-=(_FPtr fvP) {
+			for (auto it = m_toCall.begin(); it!= m_toCall.end(); it++) {
+				if(*it == fvP) {
+					m_toCall.erase(it);
+					return;
+				}
+			}
+		}
+
 	};
 }
 void abc(int a) {
 	std::cout<<"Az int: "<< a<<std::endl;
+}
+
+int*& test(int** arr){
+	return arr[2];
+
 }
 
 using namespace ch;
@@ -49,7 +66,29 @@ int main(){
 	e+=abc;
 	int x = 12;
 	e(12);*/
-	getchar();
-
+	Graphics graph(std::cout);
+	Board brd;
+	graph.setBoard(&brd);
+	/*for (int i = 0; i < 8; i++) {
+		brd.placePieceAt(Color::WHITE, Position(Column(i+1), 2), PieceType::PAWN);
+		brd.placePieceAt(Color::BLACK, Position(Column(i+1), 7), PieceType::PAWN);
+	}*/
+	brd.placePieceAt(Color::BLACK, Position(Column::CL::F, 2), PieceType::PAWN);
+	brd.placePieceAt(Color::WHITE, Position(Column::CL::G, 2), PieceType::PAWN);
+	brd.placePieceAt(Color::WHITE, Position(Column::CL::E, 1), PieceType::KING);
+	brd.placePieceAt(Color::BLACK, Position(Column::CL::E, 8), PieceType::KING);
+	graph.renderImg();
+	ChessEngine che(brd, *(King*)brd.getPieceAt(Position(Column::CL::E, 1)), *(King*)brd.getPieceAt(Position(Column::CL::E, 8)));
+	while (1) {
+		std::string in;
+		getline(std::cin, in);
+		std::istringstream ss(in);
+		unsigned fr1, fr2, de1, de2;
+		ss>>fr1>>fr2>>de1>>de2;
+		if(che.ProcessStep(Position(fr1, fr2), Position(de1, de2))){
+			graph.renderImg();
+			std::cout<<"Processed\n";
+		}
+	}
 	return 0;
 }
