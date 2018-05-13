4a60ef1e0326693b3445c99101e4b0dc49a986c0
Sutka998 <sutka998@gmail.com>
2018-05-13 02:36:55 +0200

    Hit and movement grid evaluation added to the king class

--- Chess/King.cpp
+++ Chess/King.cpp
@@ -1,5 +1,37 @@
 #include "King.h"
 
 namespace ch {
+
+	void King::m_evalHitGrid() {
+		if(m_hGridEvaled) {
+			return;
+		}
+		m_hitGrid.clear();
+		m_evalMoveGrid();
+		m_hitGrid = m_moveGrid;
+		m_hGridEvaled = true;
+		return;
+	}
+
+	void King::m_evalMoveGrid() {
+		if(m_mGridEvaled) {
+			return;
+		}
+		m_moveGrid.clear();
+		unsigned row = m_position.getRow();
+		unsigned column = m_position.getColumn().number;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if(i != 0 || j != 0) { // when 0-0, shouldn't check
+					if(row +i >= 1 && row +i <= 8 && column +j >= 1 && column+j <= 8) {
+						m_moveGrid.push_back(Position(Column(column + j), row + i));
+					}
+				}
+			}
+		}
+		m_mGridEvaled = true;
+		return;
+	}
+
 	const PieceType King::pieceType = PieceType::KING;
 }
--- Chess/King.h
+++ Chess/King.h
@@ -5,11 +5,14 @@ namespace ch {
 
 	class King: public Piece
 	{
-		void m_evalHitGrid() {}
-		void m_evalMoveGrid() {}
+		void m_evalHitGrid();
+		void m_evalMoveGrid();
 	public:
 		const static PieceType pieceType;
-		King(Color col, const Position& pos) : Piece(col, pos, PieceType::KING){}
+		King(Color col, const Position& pos) : Piece(col, pos, PieceType::KING){
+			m_evalMoveGrid();
+			m_evalHitGrid();
+		}
 		~King(void) {}
 	};
 
