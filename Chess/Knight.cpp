7150706bf9106afdc2bb9b0c7a73e46edc602ecf
Sutka998 <sutka998@gmail.com>
2018-05-13 03:15:05 +0200

    Hit and movement grid evaluation added.

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -1,3 +1,33 @@
 #include "Knight.h"
 
-namespace ch {}
+namespace ch {
+	void Knight::m_evalHitGrid() {
+		if(m_hGridEvaled) {
+			return;
+		}
+		m_hitGrid.clear();
+		m_evalMoveGrid();
+		m_hitGrid = m_moveGrid;
+		m_hGridEvaled = true;
+		return;
+	}
+
+	void Knight::m_evalMoveGrid() {
+		if(m_mGridEvaled) {
+			return;
+		}
+		m_moveGrid.clear();
+		unsigned row = m_position.getRow();
+		unsigned column = m_position.getColumn().number;
+		int nums[8][2] = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}}; //Array for easier L shape adding
+		for (int i = 0; i < 8; i++) {
+			//Checking, if the position is not overrun
+			if(row +nums[i][0] >= 1 && row +nums[i][0] <= 8 && column +nums[i][1] >= 1 && column+nums[i][1] <= 8) {
+				m_moveGrid.push_back(Position(Column(column + nums[i][1]), row + nums[i][0])); //Then add the valid ones.
+			}
+		}
+		m_mGridEvaled = true;
+		return;
+	}
+
+}
--- Chess/Knight.h
+++ Chess/Knight.h
@@ -4,11 +4,14 @@
 namespace ch {
 	class Knight : public Piece
 	{
-		void m_evalHitGrid() {}
-		void m_evalMoveGrid() {}
+		void m_evalHitGrid();
+		void m_evalMoveGrid();
 	public:
-		Knight(Color col, const Position& pos) : Piece(col, pos, PieceType::KNIGHT) {}
-		~Knight(void){}
+		Knight(Color col, const Position& pos) : Piece(col, pos, PieceType::KNIGHT) {
+			m_evalMoveGrid();
+			m_evalHitGrid();
+		}
+		~Knight(void){	}
 	};
 
 }
